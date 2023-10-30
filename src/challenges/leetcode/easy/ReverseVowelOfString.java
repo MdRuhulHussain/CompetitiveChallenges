b4d9c6a507b508273956a4cf2f2bbf303c08b10a
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-10-30 21:58:14 +0530

    Reverse or swap only the vowels of string, 2nd approach added

--- src/challenges/leetcode/easy/ReverseVowelOfString.java
+++ src/challenges/leetcode/easy/ReverseVowelOfString.java
@@ -1,5 +1,11 @@
 package challenges.leetcode.easy;
 
+/*
+ * Reversing or Swapping the vowels of a string, for e.g input = "hello" output = "holle" 
+ * or input = "a." then the output = "a."
+ * Two solutions are given below, both are of nearly same complexity, the first approach is a
+ * little more optimized 
+ */
 import java.util.Arrays;
 import java.util.List;
 
@@ -10,6 +16,10 @@ public class ReverseVowelOfString {
 		String s2 = "leetcode"; //leotcede
 		
 		System.out.println(reverseVowels(s2));
+		
+		System.out.println(reverseVowelsApproach2("a."));
+		
+		System.out.println(reverseVowelsApproach2("!!!"));
 	}
 	
 	    public static String reverseVowels(String s) {
@@ -45,6 +55,45 @@ public class ReverseVowelOfString {
 	        return String.valueOf(arr);
 
 	}
+	    
+	    
+	    public static String reverseVowelsApproach2(String s) {
+	    	int i = 0, j = s.length()-1;
+	        String resI = "", resJ = "";
+	        List<Character> vowels = Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U');
+	        boolean flagI = false, flagJ = false;
+	        while(i<j){
+	        	
+	            if(!flagI && (vowels.indexOf(s.charAt(i)) < 0)){
+	                resI += s.charAt(i);
+	                i++;
+	            } else {
+	                flagI = true;
+	            }
+
+	            if(!flagJ && (vowels.indexOf(s.charAt(j)) < 0)){
+	                resJ = s.charAt(j) + resJ;
+	                j--;
+	            } else
+	                flagJ = true;
+
+	            if(flagI && flagJ){
+	                resI += s.charAt(j);
+	                resJ = s.charAt(i)+resJ;
+	                i++;
+	                j--;
+	                flagI = false;
+	                flagJ = false;
+	            }
+	        }
+	        if(i==j)
+                resI += s.charAt(i);
+	        else if(flagI)
+	        	resI += s.charAt(i);
+	        else if(flagJ)
+	        	resJ = s.charAt(j)+resJ;
+	        return resI+resJ;
+	    }
 	    
 	    
 
