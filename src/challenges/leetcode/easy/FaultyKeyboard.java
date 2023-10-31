60c026cba96ec3ae73b22e137cc5402bf7ebd891
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-11-01 02:38:51 +0530

    2nd approach added for Faulty keyboard reverse the string on typing i

--- src/challenges/leetcode/easy/FaultyKeyboard.java
+++ src/challenges/leetcode/easy/FaultyKeyboard.java
@@ -1,14 +1,21 @@
 package challenges.leetcode.easy;
 
+/*
+ * There's a faulty keyboard, which reverses the typed string when the letter i is typed.
+ * for example if the user type string as "string", the typed values will be "rtsng"
+ */
 public class FaultyKeyboard {
 	
 	public static void main(String args[]) {
 		String s = "poiinter";
 		System.out.println(finalString(s));
+		System.out.println(finalStringWithoutInnerLoop(s));
 	}
 	
 	public static String finalString(String s) {
         String res = "";
+        StringBuilder rev = new StringBuilder("");
+        rev.append("abc");
         for(int i=0;i<s.length();i++){
             if(s.charAt(i) == 'i'){
                 String temp = "";
@@ -19,6 +26,23 @@ public class FaultyKeyboard {
                 res += s.charAt(i);
         }
         return res;
+    }
+	
+	public static String finalStringWithoutInnerLoop(String s) {
+		StringBuilder res = new StringBuilder("");
+        StringBuilder rev = new StringBuilder("");
+        for(int i =0;i<s.length();i++){
+            if(s.charAt(i)=='i'){
+                StringBuilder temp = new StringBuilder(res);
+                res = rev;
+                rev = temp;
+            }
+            else {
+                res.append(s.charAt(i));
+                rev.insert(0, s.charAt(i));
+            }
+        }
+        return res.toString();
     }
 
 }
