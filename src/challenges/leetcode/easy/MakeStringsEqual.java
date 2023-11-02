41286bec9d6bd9bece546320677ab9b41c69d42f
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-11-03 00:50:46 +0530

    Check if strings s1 and s2 can be make equals

---
+++ src/challenges/leetcode/easy/MakeStringsEqual.java
@@ -0,0 +1,40 @@
+package challenges.leetcode.easy;
+
+public class MakeStringsEqual {
+
+	public static void main(String[] args) {
+		
+		String s1 = "abcd";
+		String s2 = "cdab";
+		System.out.println(makeStringEqual(s1, s2));
+
+	}
+	
+	public static boolean makeStringEqual(String s1, String s2) {
+		if(s1.equals(s2))
+            return true;
+        int[][] arr = {{0,2},{1,3}};
+        char[] temp = s1.toCharArray();
+        for(int x=0;x<2;x++){
+        for(int i=0;i<arr.length;i++){
+            char tempChar = temp[arr[i][0]];
+            temp[arr[i][0]] = temp[arr[i][1]];
+            temp[arr[i][1]] = tempChar;
+            if(String.valueOf(temp).equals(s2))
+                return true;
+        }
+        }
+        temp = s2.toCharArray();
+        for(int x=0;x<2;x++){
+        for(int i=0;i<arr.length;i++){
+            char tempChar = temp[arr[i][0]];
+            temp[arr[i][0]] = temp[arr[i][1]];
+            temp[arr[i][1]] = tempChar;
+            if(String.valueOf(temp).equals(s1))
+                return true;
+        }
+        }
+        return false;
+	}
+
+}
