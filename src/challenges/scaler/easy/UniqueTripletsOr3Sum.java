80154e1da551eaeb08879ce9db3aa2641231ff25
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-11-18 02:52:29 +0530

    little modification in the outer loop to ignore the duplicates

--- src/challenges/scaler/easy/UniqueTripletsOr3Sum.java
+++ src/challenges/scaler/easy/UniqueTripletsOr3Sum.java
@@ -20,7 +20,7 @@ public class UniqueTripletsOr3Sum {
 		for(int i=0;i<res.size();i++) {
 			int[] triplet = res.get(i);
 			for(int n : triplet)
-				System.out.print(n+" , ");
+				System.out.print(n+" ");
 			System.out.println();
 		}
 //		res.forEach(x -> {
@@ -34,25 +34,27 @@ public class UniqueTripletsOr3Sum {
 		Arrays.sort(arr);
 		List<int[]> ans = new ArrayList<>();
 		for(int i=0;i<arr.length;i++) {
-			int[] triplet = new int[3];
-			int numToFound = target - arr[i];
-			int j=i+1, k=arr.length-1;
-			while(j<k) {
-				if(arr[j]+arr[k]==numToFound) {
-					triplet[0] = arr[i];
-					triplet[1] = arr[j];
-					triplet[2] = arr[k];
-					ans.add(triplet);
-					j++;
-					k--;
-					//ignoring the duplicates as we need to get only unique triplets
-					while(j<k && arr[j]==arr[j+1]) j++;
-					while(j<k && arr[k]==arr[k-1]) k--;
+			if(i==0 || (arr[i] != arr[i-1])) {
+				int[] triplet = new int[3];
+				int numToFound = target - arr[i];
+				int j=i+1, k=arr.length-1;
+				while(j<k) {
+					if(arr[j]+arr[k]==numToFound) {
+						triplet[0] = arr[i];
+						triplet[1] = arr[j];
+						triplet[2] = arr[k];
+						ans.add(triplet);
+						j++;
+						k--;
+						//ignoring the duplicates as we need to get only unique triplets
+						while(j<k && arr[j]==arr[j+1]) j++;
+						while(j<k && arr[k]==arr[k-1]) k--;
+					}
+					else if(arr[j]+arr[k]<numToFound)
+						j++;
+					else if(arr[j]+arr[k]>numToFound)
+						k--;
 				}
-				else if(arr[j]+arr[k]<numToFound)
-					j++;
-				else if(arr[j]+arr[k]>numToFound)
-					k--;
 			}
 		}
 		
