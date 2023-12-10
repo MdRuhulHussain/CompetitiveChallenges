f076553b90cd8ad39bd098ab1ce2fd976fbfad5a
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-12-10 23:04:23 +0530

    Sparse Array5

---
+++ src/challenges/hackerRank/medium/SparseArray.java
@@ -0,0 +1,44 @@
+package challenges.hackerRank.medium;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ * https://www.hackerrank.com/challenges/sparse-arrays/problem?isFullScreen=true
+ */
+public class SparseArray {
+	
+	public static void main(String args[]) {
+		String[] strings = {"aba", "baba", "aba", "xzxb"};
+		String[] queries = {"aba", "xzxb", "ab"};
+		int[] res = matchingStrings(strings, queries);
+		for(int num : res)
+			System.out.println(num);
+	}
+	
+	public static int[] matchingStrings(String[] strings, String[] queries) {
+	    int n,q,count=0;
+	    String s1="";
+	    String s2="";
+	    List<Integer> li=new ArrayList<Integer>();
+	    n=strings.length;
+	    q=queries.length;
+	    for(int i=0;i<q;i++)
+	    {
+	        count=0;
+	        s1=queries[i];
+	        for(int j=0;j<n;j++)
+	        {
+	            s2=strings[j];
+	            if(s1.equals(s2))
+	            count++;
+	        }
+	        li.add(count);
+	    }
+	    int[] res=new int [li.size()];
+	    for(int i=0;i<li.size();i++)
+	    res[i]=li.get(i);
+	    return res;
+	    }
+
+}
