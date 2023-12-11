7c03ed09e02c914aaf605998c7970297ead347c7
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-12-11 23:50:46 +0530

    Sparse Array - occurrence of query strings in the given array string

--- src/challenges/hackerRank/medium/SparseArray.java
+++ src/challenges/hackerRank/medium/SparseArray.java
@@ -5,6 +5,11 @@ import java.util.List;
 
 /*
  * https://www.hackerrank.com/challenges/sparse-arrays/problem?isFullScreen=true
+ * There is a collection of input strings and a collection of query strings. For each query 
+ * string, determine how many times it occurs in the list of input strings. Return an array 
+ * of the results. Example stringList = {"ab", "ab", "abc"}, queries = {"ab", "abc", "bc"}
+ * There are  instances of "ab",1  of "abc" and 0 of "bc". For each query, add an element to
+ * the return array, result = [2,1,0]
  */
 public class SparseArray {
 	
