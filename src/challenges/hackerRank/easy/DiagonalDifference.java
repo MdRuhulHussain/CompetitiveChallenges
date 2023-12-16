fc4b27233c9d1f690f01a92342f6f85aa9be77c0
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-12-17 00:17:41 +0530

    Difference of Diagonals of a square matrix

---
+++ src/challenges/hackerRank/easy/DiagonalDifference.java
@@ -0,0 +1,30 @@
+package challenges.hackerRank.easy;
+
+import java.util.List;
+
+/*
+ * Given a square matrix, calculate the absolute difference between the sums of its diagonals.
+ * For example, the square matrix  is shown below:
+ * 1 2 3
+ * 4 5 6    => for the given matrix, left-to-right diagonal = 1+5+9 = 15, right-to-left diagnal
+ * 7 8 9      = 3+5+7 = 15, The absolute difference = 0
+ */
+public class DiagonalDifference {
+	
+	public static void main(String args[]) {
+		
+		System.out.println();
+	}
+	
+	public static int diagonalDifference(List<List<Integer>> arr) {
+	    // Write your code here
+	        int m = arr.size()-1;
+	        long leftDiag = 0L, rightDiag = 0L;
+	        for(int i=0;i<arr.size();i++){
+	            leftDiag+=arr.get(i).get(i);
+	            rightDiag+=arr.get(i).get(m-i);   
+	        }
+	        return (int)Math.abs(leftDiag-rightDiag);
+	}
+
+}
