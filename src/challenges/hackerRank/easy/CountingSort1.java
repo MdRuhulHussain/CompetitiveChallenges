d2a1f96d2f66e59915a2835bb849be94584a9692
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2023-12-18 23:29:19 +0530

    Counting Sort 1

--- src/challenges/hackerRank/easy/CountingSort1.java
+++ src/challenges/hackerRank/easy/CountingSort1.java
@@ -5,10 +5,10 @@ import java.util.Arrays;
 import java.util.List;
 /*
  * the counting sort, does not require comparison. Instead, you create an integer array whose
- *  index range covers the entire range of values in your array to sort. Each time a value 
- *  occurs in the original array, you increment the counter at that index. At the end, run 
- *  through your counting array, printing the value of each non-zero valued index that number
- *  of times.for the input = [1,1,3,2,1] : The output = [0,3,1,1,0,0,0,.....99th index]
+ * index range covers the entire range of values in your array to sort. Each time a value 
+ * occurs in the original array, you increment the counter at that index. At the end, run 
+ * through your counting array, printing the value of each non-zero valued index that number
+ * of times.for the input = [1,1,3,2,1] : The output = [0,3,1,1,0,0,0,.....99th index]
  */ 
 public class CountingSort1 {
 	
