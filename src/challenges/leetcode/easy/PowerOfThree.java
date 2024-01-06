13bd9abc6b03bda9c9008069122a7c8d9dbef867
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-01-06 23:49:31 +0530

    Check if given num is a power of 3

---
+++ src/challenges/leetcode/easy/PowerOfThree.java
@@ -0,0 +1,28 @@
+package challenges.leetcode.easy;
+
+/*
+ * https://leetcode.com/problems/power-of-three/description/
+ * Given an integer n, return true if it is a power of three. Otherwise, return false.
+ * An integer n is a power of three, if there exists an integer x such that n == 3x.
+ * Example, Input : n = -1, output = false. Input : n = 9, output = true
+ */
+public class PowerOfThree {
+	
+	public static void main(String args[]) {
+		int n = 9;
+		System.out.println(isPowerOfThree(n));
+	}
+	
+	public static boolean isPowerOfThree(int n) {
+        if(n<=0)
+            return false;
+        while(n!=1){
+            if(n%3==0)
+                n/=3;
+            else
+                return false;
+        }
+        return true;
+    }
+
+}
