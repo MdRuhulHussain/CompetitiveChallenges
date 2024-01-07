376ac81c6f5c8df616415bbd684b82bfc671c592
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-01-08 01:40:02 +0530

    Power of Four, Optimized sol

--- src/challenges/leetcode/easy/PowerOfFour.java
+++ src/challenges/leetcode/easy/PowerOfFour.java
@@ -11,6 +11,8 @@ public class PowerOfFour {
 	public static void main(String args[]) {
 		int n = 16;
 		System.out.println(isPowerOfFour(n));
+		
+		System.out.println(isPowerOfFourWithoutLoopAndRecursionSol(n));
 	}
 	
 	public static boolean isPowerOfFour(int n) {
@@ -23,5 +25,9 @@ public class PowerOfFour {
         }
         return true;
     }
+	
+	public static boolean isPowerOfFourWithoutLoopAndRecursionSol(int n) {
+		return n < 1 ? false : ((n & (n-1)) == 0 && (n-1)%3==0) ? true : false;
+	}
 
 }
