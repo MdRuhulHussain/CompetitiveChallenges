533e36e574bf6f8154ea8bab163cbd5b0c61071b
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-01-10 00:27:57 +0530

    Intersection with repeated elements of Two Arrays, description and link added

--- src/challenges/leetcode/easy/IntersectionOfTwoArraysII.java
+++ src/challenges/leetcode/easy/IntersectionOfTwoArraysII.java
@@ -3,6 +3,13 @@ package challenges.leetcode.easy;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+ * https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+ * Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the 
+ * result must appear as many times as it shows in both arrays and you may return the result in any order.
+ * Example : Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2,2]
+ */
 public class IntersectionOfTwoArraysII {
 	
 	public static void main(String args[]) {
