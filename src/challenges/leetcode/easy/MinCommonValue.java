54d6363e94ceeec9eacdd195eef8b84c104f347a
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-01-13 00:01:38 +0530

    Minimum Common Value, Explaination and link added

--- src/challenges/leetcode/easy/MinCommonValue.java
+++ src/challenges/leetcode/easy/MinCommonValue.java
@@ -1,5 +1,13 @@
 package challenges.leetcode.easy;
 
+/*
+ * https://leetcode.com/problems/minimum-common-value/description/
+ * Given two integer arrays nums1 and nums2, sorted in non-decreasing order, return the minimum integer 
+ * common to both arrays. If there is no common integer amongst nums1 and nums2, return -1.
+ * Example : Input: nums1 = [1,2,3], nums2 = [2,4]
+ * Output: 2
+ * Explanation: The smallest element common to both arrays is 2, so we return 2.
+ */
 public class MinCommonValue {
 	
 	public static void main(String args[]) {
