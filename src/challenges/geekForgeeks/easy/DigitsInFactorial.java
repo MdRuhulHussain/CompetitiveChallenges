f6dca965fd7369a75ecfadffdce6d7cbd2184538
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-01-18 00:07:33 +0530

    count digits of factorial, explainantion added

--- src/challenges/geekForgeeks/easy/DigitsInFactorial.java
+++ src/challenges/geekForgeeks/easy/DigitsInFactorial.java
@@ -1,10 +1,14 @@
 package challenges.geekForgeeks.easy;
 
+/*
+ * Given an integer N. Find the number of digits that appear in its factorial. 
+ * Example : Input : N = 5. Output = 3 (as the factorial of 5 is 120)
+ */
 public class DigitsInFactorial {
 	
 	public static void main(String args[]) {
-		int n = 120;
-		System.out.println(digitsInFactorial(n));
+		int N = 120;
+		System.out.println(digitsInFactorial(N));
 	}
 	
 	public static int digitsInFactorial(int N){
