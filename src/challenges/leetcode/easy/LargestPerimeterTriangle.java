f2d8ecbe3143959dab421e19835ffaacd4c80de0
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-01-30 01:11:34 +0530

    Largest perimeter of Triangle, optimized solution

--- src/challenges/leetcode/easy/LargestPerimeterTriangle.java
+++ src/challenges/leetcode/easy/LargestPerimeterTriangle.java
@@ -13,6 +13,9 @@ public class LargestPerimeterTriangle {
 	public static void main(String args[]) {
 		int[] nums = {1,4,4,2,1,10};
 		System.out.println(largestPerimeter(nums));
+		
+		int[] nums2 = {1,4,4,2,1,10};
+		System.out.println(largestPerimeterOptimized(nums2));
 	}
 	
 	public static int largestPerimeter(int[] nums) {
@@ -30,6 +33,24 @@ public class LargestPerimeterTriangle {
                     if(nums[i]<(nums[j]+nums[k]))
                         return (nums[i]+nums[j]+nums[k]);
                 }
+            }
+        }
+        return 0;
+    }
+	
+	public static int largestPerimeterOptimized(int[] nums) {
+        Arrays.sort(nums);
+        int x = nums.length-1, temp = 0;
+        for(int i=0;i<nums.length/2;i++){
+            temp = nums[i];
+            nums[i] = nums[x];
+            nums[x] = temp;
+            x--;
+        }
+        for(int i=0;i<nums.length-2;i++){
+            for(int j=i+1;j<nums.length-1;j++){
+                if(nums[i]<nums[j]+nums[j+1])
+                    return (nums[i]+nums[j]+nums[j+1]);
             }
         }
         return 0;
