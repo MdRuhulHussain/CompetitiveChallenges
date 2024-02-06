c6cc10e32e49505e4f3b3b79ecdb3bc576debe0f
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-02-07 01:55:12 +0530

    Rearrange the cards into groups of cards in consecutive order, remove unneccessary print stmt to achieve better time complexity

--- src/challenges/leetcode/medium/HandOfStraight.java
+++ src/challenges/leetcode/medium/HandOfStraight.java
@@ -16,14 +16,14 @@ import java.util.Map;
 public class HandOfStraight {
 	
 	public static void main(String args[]) {
-//		int[] hand = {9,13,15,23,22,25,4,4,29,15,8,23,12,19,24,17,18,11,22,24,17,17,10,23,21,18,14,18,7,6,3,
-//				6,19,11,16,11,12,13,8,26,17,20,13,19,22,21,27,9,20,15,20,27,8,13,25,23,22,15,9,14,20,10,6,5,
-//				14,12,7,16,21,18};
+		int[] hand = {9,13,15,23,22,25,4,4,29,15,8,23,12,19,24,17,18,11,22,24,17,17,10,23,21,18,14,18,7,6,3,
+				6,19,11,16,11,12,13,8,26,17,20,13,19,22,21,27,9,20,15,20,27,8,13,25,23,22,15,9,14,20,10,6,5,
+				14,12,7,16,21,18};
 		
-		int[] hand = {1,2,3,6,4,2,3,2,3,4,7,8};
+//		int[] hand = {1,2,3,6,4,2,3,2,3,4,7,8};
 		
 		int groupSize = 10;
-		groupSize = 3;
+//		groupSize = 3;
 		System.out.println(isNStraightHand(hand, groupSize));
 		
 	}
@@ -43,12 +43,12 @@ public class HandOfStraight {
             }
         }
         int groupCount = hand.length/groupSize;
-        System.out.println(hand.length+"--1--"+groupCount);
+//        System.out.println(hand.length+"--1--"+groupCount);
         while(groupCount>0){
             int start = min;
             int end = min+groupSize;
             boolean minFound = false;
-            System.out.println(min+"-----"+groupCount);
+//            System.out.println(min+"-----"+groupCount);
             while(start<end){
                 if(!freq.containsKey(start))
                     return false;
@@ -60,7 +60,7 @@ public class HandOfStraight {
                     freq.remove(start);
                 start++;
             }
-            System.out.println(start);
+//            System.out.println(start);
             groupCount--;
             if(!minFound && groupCount>0){
                 start--;
