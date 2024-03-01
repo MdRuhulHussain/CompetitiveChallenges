9f392e7ce734a8b3792dab49ea873b3c580e98d8
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-03-01 23:33:47 +0530

    Check if the given string can be palindrome after deleting at most one character from it

--- src/challenges/leetcode/easy/Palindrom2.java
+++ src/challenges/leetcode/easy/Palindrom2.java
@@ -2,19 +2,11 @@ package challenges.leetcode.easy;
 
 /*
  * Given a string s, return true if the s can be palindrome after deleting at most one character from it.
- * Example 1:
-
-Input: s = "aba"
-Output: true
-Example 2:
-
-Input: s = "abca"
-Output: true
-Explanation: You could delete the character 'c'.
-Example 3:
-
-Input: s = "abc"
-Output: false
+ * Example 1: Input: s = "aba"; Output: true
+ * Example 2: Input: s = "abca"; Output: true
+ * Explanation: You could delete the character 'c'.
+ * Example 3:
+ * Input: s = "abc"; Output: false
  */
 public class Palindrom2 {
 	
