9094917286c198a0242fa38f37e5bb3c31be54d8
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-03-28 01:24:07 +0530

    Find the duplicate number

---
+++ src/challenges/leetcode/medium/FindDuplicateNumber.java
@@ -0,0 +1,33 @@
+package challenges.leetcode.medium;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+ * Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+ * There is only one repeated number in nums, return this repeated number.
+ * You must solve the problem without modifying the array nums and uses only constant extra space.
+ * Example 1: Input: nums = [1,3,4,2,2]; Output: 2
+ * Example 2: Input: nums = [3,1,3,4,2]; Output: 3
+ * Example 3: Input: nums = [3,3,3,3,3]; Output: 3
+ */
+public class FindDuplicateNumber {
+	
+	public static void main(String args[]) {
+		int[] nums = {2,5,3,3,3,4,8};
+		System.out.println(findDuplicate(nums));
+	}
+	
+	public static int findDuplicate(int[] nums) {
+        
+         Set<Integer> unique = new HashSet<>();
+         for(int num : nums){
+             if(unique.contains(num))
+                 return num;
+             unique.add(num);
+         }
+         return 0;
+    }
+	
+
+}
