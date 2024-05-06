d012c83ecbd988bb6188753ec4d31514a57e9257
Mohammed Ruhul Hossain <heusenruhul@gmail.com>
2024-05-07 01:52:36 +0530

    Find all pairs whose sum = X, modified printstmt and added description

--- src/challenges/geekForgeeks/easy/FindAllPairsWSumX.java
+++ src/challenges/geekForgeeks/easy/FindAllPairsWSumX.java
@@ -4,6 +4,15 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/*
+ * Given two unsorted arrays A of size N and B of size M of distinct elements, the task is to find all pairs from both arrays whose sum is equal to X.
+ * Note: All pairs should be printed in increasing order of u. For eg. for two pairs (u1,v1) and (u2,v2), if u1 < u2 then (u1,v1) should be printed first else second.
+ * Example 1: Input: A[] = {1, 2, 4, 5, 7}, B[] = {5, 6, 3, 4, 8}, X = 9 
+ * Output: 1 8, 4 5 , 5 4
+ * Explanation: (1, 8), (4, 5), (5, 4) are the pairs which sum to 9.
+ * Example 2: Input: A[] = {-1, -2, 4, -6, 5, 7}, B[] = {6, 3, 4, 0} , X = 8 
+ * Output: 4 4 , 5 3
+ */
 public class FindAllPairsWSumX {
 	
 	public static void main(String args[]) {
@@ -12,7 +21,7 @@ public class FindAllPairsWSumX {
 		long X = 8;
 		pair[] pairs = allPairs(A, B, A.length, B.length, X);
 		for(pair p : pairs)
-			System.out.println(p.first+" "+p.second);
+			System.out.println("first = "+p.first+" second = "+p.second);
 	}
 	
 	public static pair[] allPairs( long A[], long B[], long N, long M, long X) {
